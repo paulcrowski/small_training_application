ff4c2ee84d5e52b4366445aeb768c1997c24fbe2
Paul W <p.p.wronski@gmail.com>
2015-12-03 12:04:49 +0100

    tooltip project

--- tooltip/js/app.js
+++ tooltip/js/app.js
@@ -4,4 +4,29 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("dziala !!!");
 
+    var tooltipList = document.getElementsByClassName("tooltip");
+    var tooltipElement = null;
+
+    for ( var i=0; i < tooltipList.length; i++) {
+        tooltipList[i].addEventListener("mouseover", function (event) {
+
+            tooltipElement = document.createElement("div");
+            var toolTipText = this.dataset.tooltip;
+
+            tooltipElement.innerHTML = toolTipText;
+            this.appendChild(tooltipElement);
+            toolTipElement.classList.add("otherclass");
+        });
+    }
+
+    for ( var i=0; i < tooltipList.length; i++) {
+        tooltipList[i].addEventListener("mouseout", function (event) {
+            console.log("mouse out");
+            this.removeChild(tooltipElement);
+            tooltipElement = null;
+            toolTipElement.classList.remove("otherclass");
+        });
+    }
+
+
 });
