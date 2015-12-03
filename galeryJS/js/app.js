03eef97ea3dfbe4765aac5d291620c8522ab68b2
Paul W <p.p.wronski@gmail.com>
2015-12-03 10:46:25 +0100

    slider with 3 pictures ready

--- galeryJS/js/app.js
+++ galeryJS/js/app.js
@@ -3,4 +3,30 @@
  */
 document.addEventListener("DOMContentLoaded", function () {
    console.log("dziala !!!");
+   var listWithPictures = document.querySelectorAll("#gallery li");
+   var nextButton = document.getElementById("nextPicture");
+   var prevButton = document.getElementById("prevPicture");
+   var visiblePicture = 0;
+   listWithPictures[visiblePicture].classList.remove("invisible");
+
+   nextButton.addEventListener("click", function(event) {
+      console.log('click');
+      listWithPictures[visiblePicture].classList.add("invisible");
+      visiblePicture++;
+      if (visiblePicture >= listWithPictures.length) {
+         visiblePicture = 0;
+      }
+      listWithPictures[visiblePicture].classList.remove("invisible");
+   });
+
+   prevButton.addEventListener("click", function(event) {
+      console.log('click');
+      listWithPictures[visiblePicture].classList.add("invisible");
+      visiblePicture--;
+      if (visiblePicture < 0) {
+         visiblePicture = 2;
+      }
+      listWithPictures[visiblePicture].classList.remove("invisible");
+
+   });
 });
