4c2cdbdd7e9017e3f0c9e49474cc0f3621d46d98
Paul W <p.p.wronski@gmail.com>
2015-12-04 14:13:44 +0100

    acordeon ready 100%

--- acordeon/js/app.js
+++ acordeon/js/app.js
@@ -1,7 +1,32 @@
 /**
  * Created by POL on 2015-12-04.
  */
+
+// naprostsza rzecz
+//$(document).ready(function(){
+//    console.log('dziala');
+//    var acordeonHeadings = $(".acordeon h1");
+//
+//    acordeonHeadings.on("click", function(event){
+//        var paragraf = $(this).next();
+//        paragraf.toggle(1000);
+//    });
+//
+//});
+
 $(document).ready(function(){
-    console.log('dziala');
+        console.log('dziala');
+        var acordeonHeadings = $(".acordeon h1");
+        var currentVisibleElement = null;
+
+        acordeonHeadings.on("click", function(event){
+            if (currentVisibleElement != null && currentVisibleElement.get(0) !=
+                $(this).next().get(0)) {
+                currentVisibleElement.hide(1000);
+            }
+
+            currentVisibleElement = $(this).next();
+            currentVisibleElement.show(1000);
+        });
 
 });
