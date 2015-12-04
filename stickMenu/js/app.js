13c95750598e1f83acc44c8cb11a4f62da0cac2c
Paul W <p.p.wronski@gmail.com>
2015-12-04 09:36:46 +0100

    stickbar ready to use, still problem with aritcle element we need to add empty div later

--- stickMenu/js/app.js
+++ stickMenu/js/app.js
@@ -3,4 +3,17 @@
  */
 $(document).ready(function(){
     console.log('dziala');
+    var menu = $(".menu");
+    var menuOffsetFromTop = menu.position().top;
+
+    $(window).on("scroll", function(event){
+        console.log($(window).scrollTop());
+        console.log(menu.position());
+        if($(window).scrollTop()> menuOffsetFromTop) {
+            menu.addClass("sticky");
+        }
+        else {
+            menu.removeClass("sticky");
+        }
+    });
 });
